199a7759fe29b0fb8b5686b7aec4dd82951348aa
Student <s1091828@gm.pu.edu.tw>
2022-04-19 11:18:17 +0800

    v1: 全螢幕觸控進行畫圓

---
+++ app/src/main/java/com/example/multitouch/Fingers.kt
@@ -0,0 +1,29 @@
+package com.example.multitouch
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+class Fingers(context: Context?) : View(context) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var xPos:Float = 200f
+    var yPos:Float = 200f
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.LTGRAY)
+        paint.color = Color.YELLOW
+        canvas.drawCircle(xPos, yPos, 80f, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean{
+        xPos = event.getX()
+        yPos = event.getY()
+        invalidate()
+        return true
+    }
+
+}
